a57b6d9ad163dacd0c9130cd4c4c6b5ecd7df546
ShiryaevNikolay <deendeeps@list.ru>
2020-05-04 14:40:21 +0300

    Dynamic change icon of FAB

--- app/src/main/java/com/example/schedule/AddScheduleActivity.kt
+++ app/src/main/java/com/example/schedule/AddScheduleActivity.kt
@@ -4,9 +4,15 @@ import android.app.Activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
+import android.view.View
+import androidx.core.widget.addTextChangedListener
 import kotlinx.android.synthetic.main.activity_add_item.*
 
-class AddScheduleActivity : AppCompatActivity(), MenuItem.OnMenuItemClickListener {
+class AddScheduleActivity : AppCompatActivity(), View.OnClickListener, MenuItem.OnMenuItemClickListener {
+
+    private var lesson = ""
+    private var teacher = ""
+    private var auditorium = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,6 +25,18 @@ class AddScheduleActivity : AppCompatActivity(), MenuItem.OnMenuItemClickListene
             setResult(Activity.RESULT_CANCELED)
             finish()
         }
+
+        fab.setOnClickListener(this)
+        btn_start_time_schedule.setOnClickListener(this)
+        btn_end_time_schedule.setOnClickListener(this)
+        btn_week_schedule.setOnClickListener(this)
+
+        et_lesson_schedule.addTextChangedListener {
+            lesson = it.toString()
+            checkMandatoryItem()
+        }
+        et_teacher_schedule.addTextChangedListener { teacher = it.toString() }
+        et_auditorium_schedule.addTextChangedListener { auditorium = it.toString() }
     }
 
     override fun onBackPressed() {
@@ -29,5 +47,24 @@ class AddScheduleActivity : AppCompatActivity(), MenuItem.OnMenuItemClickListene
 
     override fun onMenuItemClick(item: MenuItem?): Boolean {
         TODO("Not yet implemented")
+    }
+
+    override fun onClick(v: View?) {
+        when (v?.id) {
+            R.id.fab -> {
+                if (lesson == "") {
+                    setResult(Activity.RESULT_CANCELED)
+                    finish()
+                }
+            }
+        }
+    }
+
+    private fun checkMandatoryItem() {
+        if (lesson != "") {
+            fab.setImageResource(R.drawable.ic_done_fab)
+        } else if (lesson == "") {
+            fab.setImageResource(R.drawable.ic_back_fab)
+        }
     }
 }
