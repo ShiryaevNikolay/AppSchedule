ca823726a010b82ce1f0c769cf0630a7106d4523
ShiryaevNikolay <deendeeps@list.ru>
2020-05-08 14:21:42 +0300

    Fixed show background of item when swipe

--- app/src/main/java/com/example/schedule/modules/SwipeDragItemHelper.kt
+++ app/src/main/java/com/example/schedule/modules/SwipeDragItemHelper.kt
@@ -10,6 +10,7 @@ import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.RecyclerView
 import com.example.schedule.R
 import com.example.schedule.interfaces.ItemTouchHelperListener
+import it.xabaras.android.recyclerview.swipedecorator.RecyclerViewSwipeDecorator
 import kotlinx.android.synthetic.main.item_schedule_rv.view.*
 
 class SwipeDragItemHelper(
@@ -45,23 +46,19 @@ class SwipeDragItemHelper(
         isCurrentlyActive: Boolean
     ) {
         super.onChildDraw(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive)
-        if (dX != 0f && isCurrentlyActive) {
-            val itemView = viewHolder.itemView
-            val icon = ContextCompat.getDrawable(context, R.drawable.ic_trash)
-            val background: GradientDrawable = ContextCompat.getDrawable(context, R.drawable.bg_swipe_item_rv) as GradientDrawable
-            val top = itemView.top + (itemView.height - icon!!.intrinsicHeight) / 2
-            val bottom = top + icon.intrinsicHeight
 
-            if (dX < 0) {
-                val left = itemView.right - icon.intrinsicWidth - (itemView.height - icon.intrinsicHeight) / 2
-                val right = itemView.right - (itemView.height - icon.intrinsicHeight) / 2
-                icon.setBounds(left, top, right, bottom)
-                background.setBounds(itemView.right + dX.toInt() - 20, itemView.top, itemView.right, itemView.bottom)
-            } else {
-                background.setBounds(0, 0, 0, 0)
-            }
-            background.draw(c)
-            icon.draw(c)
-        }
+        val itemView = viewHolder.itemView
+        val background: GradientDrawable = ContextCompat.getDrawable(context, R.drawable.bg_swipe_item_rv) as GradientDrawable
+        background.setBounds(itemView.left, itemView.top, itemView.right, itemView.bottom)
+        background.draw(c)
+
+        RecyclerViewSwipeDecorator.Builder(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive)
+            .addSwipeLeftLabel(context.resources.getString(R.string.btn_remove).toString())
+            .addSwipeRightLabel(context.resources.getString(R.string.btn_remove).toString())
+            .setSwipeLeftLabelColor(ContextCompat.getColor(context, R.color.white))
+            .setSwipeRightLabelColor(ContextCompat.getColor(context, R.color.white))
+            .addActionIcon(R.drawable.ic_trash)
+            .create()
+            .decorate()
     }
 }
